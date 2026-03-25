64f8fa8054254f0f641fadc3ba25e6dd2251ce38
samuelburnham <45365069+samuelburnham@users.noreply.github.com>
2026-03-25 12:06:21 -0400

    chore: Update SP1 to v6.0.2, add CI

--- templates/hello-world/flake.nix
+++ templates/hello-world/flake.nix
@@ -1,9 +1,18 @@
 {
-  description = "A very basic flake";
+  description = "SP1 template Nix flake";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
 
   inputs = {
     # System packages
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.follows = "sp1/nixpkgs";
 
     # Helper: flake-parts for easier outputs
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -14,8 +23,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sp1 = {
-      url = "github:argumentcomputer/sp1.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # url = "github:argumentcomputer/sp1.nix";
+      url = "path:../..";
     };
   };
 
@@ -46,7 +55,7 @@
             file = ./rust-toolchain.toml;
             # Update this hash when `rust-toolchain.toml` changes
             # Just copy the expected hash from the `nix build` error message
-            sha256 = "sha256-sqSWJDUxc+zaz1nBWMAJKTAGBuGWP25GCftIOlCEAtA=";
+            sha256 = "sha256-SBKjxhC6zHTu0SyJwxLlQHItzMzYZ71VCWQC2hOzpRY=";
           };
         };
       in {
